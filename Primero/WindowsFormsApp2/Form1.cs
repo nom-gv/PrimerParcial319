42d78bf3154f20d846549c2d241e1aba35c4e584
Noemi <noemi.guarachi@gmail.com>
2021-09-23 12:27:52 -0400

    Primera subida

---
+++ Primero/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void numeros(object sender, EventArgs e)
+        {
+            Button botonActual = (Button)sender;
+            Resultado.Text = Resultado.Text + botonActual.Text;
+            
+        }
+
+        private void operacion(object sender, EventArgs e)
+        {
+            Button botonActual = (Button)sender;
+            Resultado.Text = Resultado.Text + botonActual.Text;
+        }
+
+        private void borrar(object sender, EventArgs e)
+        {
+            String result = Resultado.Text;
+            if (result != "")
+                result = result.Substring(0, result.Length - 1);
+            Resultado.Text = result;
+        }
+
+        private void eliminar(object sender, EventArgs e)
+        {
+            Resultado.Text = "";
+            Total.Text = "0";
+        }
+
+        private void igual(object sender, EventArgs e)
+        {
+            String result = Resultado.Text;
+            if (result != "")
+            {
+                if("0123456789".IndexOf(result.Substring(result.Length - 1, 1)) > -1)
+                {
+                    var respuesta = Evaluar(result);
+                    Total.Text = respuesta.ToString();
+                }
+            }   
+        }
+
+        private static Double Evaluar(String expression)
+        {
+            //Creo un DataTable
+            System.Data.DataTable table = new System.Data.DataTable();
+            //Realizo el cálculo..
+            object result = table.Compute(expression, string.Empty);
+            //Lo devuelvo convertido a Double
+            return Convert.ToDouble(result);
+        }
+    }
+}
---
+++ Quinto/ConsoleApplication2/Program.fs
@@ -0,0 +1,25 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+
+[<EntryPoint>]
+let main argv = 
+    
+    let primero = 2
+    let segundo = 10
+
+    //PRIMERA FORMA
+    let sumassuc = Seq.init segundo (fun n -> (n * primero)+primero) 
+    let ult = Seq.last sumassuc
+
+    //SEGUNDA FORMA
+    let mutable ultimo = 0
+    for n in 1..segundo do
+        ultimo <- ultimo + primero
+        printfn "%i+%i=%i" primero n ultimo
+
+    printfn "Multiplicacion de %i*%i = %i" primero segundo ultimo
+
+
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
