0c9d5bb134338b0b45c61cdf558addeb05ea9668
Noemi <noemi.guarachi@gmail.com>
2021-09-23 13:25:27 -0400

    Primera subida

---
+++ Cuarto/ConsoleApplication1/Program.fs
@@ -0,0 +1,14 @@
+﻿// Learn more about F# at http://fsharp.org
+// See the 'F# Tutorial' project for more help.
+open System
+[<EntryPoint>]
+let main argv = 
+
+    let lista = [1;4;2;5;1;3;6;8;7;4]
+    let impares list = List.filter (fun x -> (x+1) % 2 = 0) list 
+    let pares list = List.filter (fun x -> x % 2 = 0) list 
+    printfn "Lista de Impares: %A" (impares lista)
+    printfn "Lista de Pares: %A" (pares lista)
+    
+    Console.ReadKey() |> ignore
+    0 // return an integer exit code
